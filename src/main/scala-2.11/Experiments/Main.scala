95a83a109c2a687c9ac8128f94f6ee170ecc04d7
GeorgeM <giwrgosmandi@gmail.com>
2019-11-14 09:53:15 +0200

    fixing bugs regarding dirty er

--- src/main/scala-2.11/Experiments/Main.scala
+++ src/main/scala-2.11/Experiments/Main.scala
@@ -3,9 +3,9 @@ package Experiments
 import java.util.Calendar
 
 import SparkER.DataStructures.Profile
-import SparkER.Wrappers.{CSVWrapper, JSONWrapper, SerializedObjectLoader}
-import org.apache.log4j.{FileAppender, Level, LogManager, SimpleLayout}
-import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+import SparkER.Wrappers.{CSVWrapper, JSONWrapper}
+import org.apache.log4j.{LogManager, SimpleLayout}
+import org.apache.spark.{ SparkConf, SparkContext}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.spark.rdd.RDD
@@ -20,12 +20,11 @@ import org.apache.spark.rdd.RDD
 object Main {
 
 
-// TODO Fix main to support dirty ER
 
   def main(args: Array[String]): Unit = {
 
-    Logger.getLogger("org").setLevel(Level.ERROR)
-    Logger.getLogger("akka").setLevel(Level.ERROR)
+    // Logger.getLogger("org").setLevel(Level.ERROR)
+    // Logger.getLogger("akka").setLevel(Level.ERROR)
     val log = LogManager.getRootLogger
     log.setLevel(Level.INFO)
 
@@ -35,7 +34,7 @@ object Main {
       .setAppName("SparkER")
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(conf)
-    
+
     // Parsing the input arguments
     def nextOption(map: OptionMap, list: List[String]): OptionMap = {
         list match {
@@ -94,27 +93,38 @@ object Main {
       if (options.contains("bcstep") &&  isAllDigits(options("bcstep") ))
         options("bcstep").toInt
     else 0
-    log.info("SPARKER - Broadcast Step " + bcstep)
+    if (bcstep > 0 ) log.info("SPARKER - Broadcast Step " + bcstep)
 
-    val (profiles, newGT, separators) =
-      if (options.contains("d2"))
-        cleanCleanER(options, log)
-      else
-        dirtyER(options, log)
+    // choose between clean-clean or dirty ER
+    if (options.contains("d2")) {
+      val (profiles, newGT, separators) = cleanCleanER(options, log)
+      val maxProfileID = profiles.map(_.id).max()
+      val maxIdDataset1 = profiles.filter(_.sourceId == 1).map(_.id).max()
+      val newGTSize = newGT.size
+      val gt = sc.broadcast(newGT)
+      EntityResolution.resolution(log, separators, profiles, startTime, maxProfileID, gt, newGTSize, maxIdDataset1,  metablocking, bcstep)
 
-    val newGTSize = newGT.size
-    val gt = sc.broadcast(newGT)
+    } else {
+      val (profiles, newGT) = dirtyER(options, log)
+      val newGTSize = newGT.size
+      val gt = sc.broadcast(newGT)
+      val maxProfileID = profiles.map(_.id).max()
+      EntityResolution.resolution(log, Array(), profiles, startTime, maxProfileID, gt, newGTSize, -1,  metablocking, bcstep)
+    }
 
-    val maxProfileID = profiles.map(_.id).max()
-    val maxIdDataset1 = profiles.filter(_.sourceId == 1).map(_.id).max()
-
-    EntityResolution.resolution(log, separators, profiles, startTime, maxProfileID, gt, newGTSize, maxIdDataset1,  metablocking, bcstep)
-
+    System.in.read()
     sc.stop()
   }
 
 
-
+  /**
+    * Parse the input arguments and calculate the ncessary variables for
+    * Clean-Clean ER execution.
+    *
+    * @param options input command line arguments
+    * @param log logger
+    * @return the loaded profiles as RDD, the loaded ground truth and an array of separators
+    */
   def cleanCleanER(options : Map[String, String], log : Logger) : (RDD[Profile], Set[(Long, Long)], Array[Long]) ={
     val sc = SparkContext.getOrCreate()
 
@@ -216,7 +226,15 @@ object Main {
 
 
 
-  def dirtyER(options : Map[String, String], log : Logger) : (RDD[Profile], Set[(Long, Long)], Array[Long]) ={
+  /**
+    * Parse the input arguments and calculate the ncessary variables for
+    * Dirty ER execution.
+    *
+    * @param options input command line arguments
+    * @param log logger
+    * @return the loaded profiles as RDD and the loaded ground truth
+    */
+  def dirtyER(options : Map[String, String], log : Logger) : (RDD[Profile], Set[(Long, Long)]) ={
     val sc = SparkContext.getOrCreate()
 
     val separator =
@@ -230,8 +248,8 @@ object Main {
     val datasetExtension1 = dataset1Path.toString.split("\\.").last
     val dataset1 =
       datasetExtension1 match {
-        case "csv" => CSVWrapper.loadProfiles2(dataset1Path, realIDField = "id", separator = separator, sourceId = 1, header = true)
-        case "json" => JSONWrapper.loadProfiles(dataset1Path, realIDField = "realProfileID", sourceId = 1)
+        case "csv" => CSVWrapper.loadProfiles2(dataset1Path, separator = separator, header = true)
+        case "json" => JSONWrapper.loadProfiles(dataset1Path)
         case _ => {
           log.error("SPARKER - This filetype is not supported yet")
           System.exit(1)
@@ -239,9 +257,6 @@ object Main {
         }
       }
 
-    val maxIdDataset1 = dataset1.map(_.id).max()
-
-    val separators = Array(maxIdDataset1)
     var profiles = dataset1
     if (options.contains("partitions"))  profiles = profiles.repartition(options("partitions").toInt)
     profiles.setName("Profiles").cache()
@@ -253,7 +268,6 @@ object Main {
 
     // Reading Ground-Truth dataset
     val groundtruthPath = options("gt")
-
     val gtExtension2 = groundtruthPath.toString.split("\\.").last
     val groundtruth = gtExtension2 match {
       case "csv" => CSVWrapper.loadGroundtruth(groundtruthPath, separator, header = true)
@@ -265,34 +279,20 @@ object Main {
       }
     }
 
-    //Converts the ids in the groundtruth to the autogenerated ones
-    val realIdIds1 = sc.broadcast(dataset1.map { p =>
-      (p.originalID, p.id)
-    }.collectAsMap())
-
     var newGT: Set[(Long, Long)] = null
     newGT = groundtruth.map { g =>
-      val first = realIdIds1.value.get(g.firstEntityID)
-      val second = realIdIds1.value.get(g.secondEntityID)
-      if (first.isDefined && second.isDefined) {
-        val f = first.get
-        val s = second.get
-        if (f < s) {
-          (f, s)
-        }
-        else {
-          (s, f)
-        }
-      }
-      else {
-        (-1L, -1L)
-      }
+      val f = g.firstEntityID.toLong
+      val s = g.secondEntityID.toLong
+      if (f < s)
+        (f, s)
+      else
+        (s, f)
     }.filter(_._1 >= 0).collect().toSet
 
     val gtTime = Calendar.getInstance()
     log.info("SPARKER - Time to load groundtruth " + (gtTime.getTimeInMillis - pTime.getTimeInMillis) / 1000.0 / 60.0 + " min")
 
-    (profiles, newGT, separators)
+    (profiles, newGT)
 
   }
 
